6162a102108ea84f003f39315c7bf9aea654d512
DavidChen0429 <chenzk0429@gmail.com>
2023-10-01 11:18:22 +0200

    Add hellow world

---
+++ HelloWorld/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Yup, welcome to C++, overcome your fear for coding" << endl;
+    return 0;
+}
